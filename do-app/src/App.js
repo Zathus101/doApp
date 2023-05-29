fa4f2622b33757aa1d7c3d7c98b8f2b4c200f7fb
Zaki Mirimiri <zakimirimiri@Zakis-MacBook-Pro.local>
2023-05-29 18:07:24 -0400

    first commit

---
+++ do-app/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div class="title">To Do List</div>
+        
+      </header>
+
+      <div class="information">
+        <label for="first">Enter Your First Name</label>
+      </div>
+    </div>
+  );
+}
+
+export default App;
